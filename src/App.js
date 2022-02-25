745475b5e097d538ad1a9dd4c34c6ecb8c027a36
aratrika1996 <aratrika.mukherjee26@gmail.com>
2022-02-25 15:53:03 +0530

    Test1

--- src/App.js
+++ src/App.js
@@ -2,31 +2,41 @@ import React, { useEffect } from "react";
 import { isAndroid, isIOS } from "react-device-detect";
 
 const App = () => {
-  useEffect(() => {
+  // useEffect(() => {
+  //   if (isAndroid) {
+  //     const url =
+  //       "intent://metamask.io/#Intent;scheme=https;package=io.metamask;end";
+
+  //     window.location.replace(url);
+  //   } else if (isIOS) {
+  //     window.location.replace("instagram://");
+
+  //     setTimeout(() => {
+  //       window.location.replace(
+  //         "https://apps.apple.com/us/app/instagram/id389801252"
+  //       );
+  //     }, 10000);
+  //   } else {
+  //     window.location.replace("https://metamask.io");
+  //   }
+  // }, []);
+
+  const deepLink = () => {
     if (isAndroid) {
-      const url =
-        "intent://metamask.io/#Intent;scheme=https;package=io.metamask;end";
-
-      window.location.replace(url);
-    } else if (isIOS) {
-      window.location.replace("instagram://");
-
-      setTimeout(() => {
-        window.location.replace(
-          "https://apps.apple.com/us/app/instagram/id389801252"
-        );
-      }, 10000);
-    } else {
-      window.location.replace("https://metamask.io");
+          const url =
+            "intent://metamask.io/#Intent;scheme=https;package=io.metamask;end";
+    
+          window.location.replace(url);
     }
-  }, []);
+    else {
+      window.location.replace("https://metamask.io")
+    }
+  }
 
   return (
     <div className="App">
-      <div>
-        If you have not been automatically redirected, click on the following link:
-      </div>
-      {isAndroid ? (
+      <button className="wallet" onClick={deepLink}>Connect Wallet</button>
+      {/* {isAndroid ? (
         <a href="https://play.google.com/store/apps/details?id=io.metamask">
           Open Android app
         </a>
@@ -36,7 +46,7 @@ const App = () => {
         </a>
       ) : (
         <a href="https://instagram.com">Open Web app</a>
-      )}
+      )} */}
     </div>
   );
 };
